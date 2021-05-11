6eb35c3e945cbe6bc9f47a9bde220e0cf3ca5ab3
kouheidesu <gyarmex@gmail.com>
2021-05-11 18:30:19 +0900

    20210511 kazihara

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   const target = document.getElementById("menu");
   target.classList.toggle('open');
-  const nav = document.getElementB
-  yId("nav");
+  const nav = document.getElementById("nav");
   nav.classList.toggle('in');
 });
 
