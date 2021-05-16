5e36bd138a83d864a800768bbcee720bba01f5fe
kouheidesu <gyarmex@gmail.com>
2021-05-16 10:33:35 +0900

    20210516 kazihara

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ target.addEventListener('click', () => {
   nav.classList.toggle('in');
 });
 
+
+  function showBalloon(){ var wObjballoon = document.getElementById("makelmg");
+    if(wObjballoon.className == "balloonl"){
+      wObjballoon.className = "balloon";
+    }else{
+      wObjballoon.className = "balloonl";
+    }
+  }
+
+  
+
+  $(document).ready(function(){
+    
+    $('#example').darkTooltip();
+
+    $('#light').darkToolp({
+      animation:'flipin',
+      gravity:'west',
+      confirm:true,
+      theme:'light',
+      trigger:'click'
+    });
+  });
